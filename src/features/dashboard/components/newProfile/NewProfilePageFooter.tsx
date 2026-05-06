ee3b4ec7cf39cfc9c5b62bb5bc747e4db12d23ee
Shihab177 <shihab7777h@gmail.com>
2026-05-07 00:09:09 +0600

    feat :  add submitProfile  event

--- src/features/dashboard/components/newProfile/NewProfilePageFooter.tsx
+++ src/features/dashboard/components/newProfile/NewProfilePageFooter.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+"use client"
+import { useProfileStore } from "@/store/createNewProfileStore";
+import React from "react";
 
 const NewProfilePageFooter = () => {
-    return (
-        <div className='sticky bottom-0 z-50 border-t border-gray-100 bg-[#FFFFFF] shadow-[0_0_2px_rgba(0,0,0,0.2)] p-2 '>
-          <button className='bg-blue-600 py-2 px-7 text-white rounded-md text-md font-semibold mr-6 ml-20'>ok</button>
-          <button className='text-md font-semibold text-gray-400 hover:text-blue-500'>Cancel</button>
-        </div>
-    );
+  const submitProfile = useProfileStore((state) => state.submitProfile);
+  return (
+    <div className="sticky bottom-0 z-50 border-t border-gray-100 bg-[#FFFFFF] shadow-[0_0_2px_rgba(0,0,0,0.2)] p-2 ">
+      <button
+        onClick={submitProfile}
+        className="bg-blue-600 py-2 px-7 text-white rounded-md text-md font-semibold mr-6 ml-20"
+      >
+        ok
+      </button>
+      <button className="text-md font-semibold text-gray-400 hover:text-blue-500">
+        Cancel
+      </button>
+    </div>
+  );
 };
 
 export default NewProfilePageFooter;
