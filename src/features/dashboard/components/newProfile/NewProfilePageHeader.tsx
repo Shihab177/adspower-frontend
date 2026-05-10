0d4dbd5b63cede5246b1bbde4f3813bbb8822067
Shihab177 <shihab7777h@gmail.com>
2026-05-10 11:06:45 +0600

    feat : add smooth scrollIntoView

--- src/features/dashboard/components/newProfile/NewProfilePageHeader.tsx
+++ src/features/dashboard/components/newProfile/NewProfilePageHeader.tsx
@@ -1,18 +1,30 @@
-
 import { newProfileMenuItems } from "@/constants/data";
 import React, { useState } from "react";
 interface NewProfilePageHeaderProps {
   activeTab: string;
   setActiveTab: (tab: string) => void;
 }
-const NewProfilePageHeader = ({ activeTab, setActiveTab }: NewProfilePageHeaderProps) => {
-  
+const NewProfilePageHeader = ({
+  activeTab,
+  setActiveTab,
+}: NewProfilePageHeaderProps) => {
+  const handleTabClick = (tabId: string) => {
+    setActiveTab(tabId); 
+
+    const element = document.getElementById(tabId);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth", 
+        block: "start", 
+      });
+    }
+  };
   return (
     <div className="flex gap-8 border-b border-gray-200 px-3">
       {newProfileMenuItems.map((item) => (
         <button
           key={item.id}
-          onClick={() => setActiveTab(item.id)}
+          onClick={() => handleTabClick(item.id)}
           className={`pb-3 text-md font-semibold transition-all duration-200 border-b-2 ${
             activeTab === item.id
               ? "text-blue-600 border-blue-600"
